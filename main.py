8504bbab25a761b216e4df92a69ba0c18b5311d2
Dioxane123 <1204876262@qq.com>
2026-01-20 16:38:26 +0800

    可视化了特征图

--- main.py
+++ main.py
@@ -1,40 +1,42 @@
 import torch
-import timm
 from PIL import Image
-from timm.data import resolve_data_config
+import numpy as np
+import matplotlib.pyplot as plt
+import timm
+from timm.data import resolve_model_data_config
 from timm.data.transforms_factory import create_transform
+from sklearn.decomposition import PCA
+pca = PCA(n_components=3)
 
-# 1. 加载一张图片 (请替换为你的本地图片路径)
-# 这里为了演示，假设有一张图片对象
-# img = Image.open("path/to/your/image.jpg").convert('RGB')
-# 如果没有图片，我们可以生成一个随机的伪造图片用于演示
-img = Image.new('RGB', (224, 224), color='red')
-
-# 2. 创建模型
-# 'resnet50' 可以替换为其他模型名，如 'convnext_tiny', 'vit_base_patch16_224'
-# pretrained=True: 加载在 ImageNet 上预训练的权重
-# num_classes=0: 关键参数！这将移除全连接层，直接输出特征向量
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = timm.create_model('vit_base_patch16_dinov3.lvd1689m', pretrained=True, features_only=True)
+model.to(device)
+model.eval()
 
-# 3. 设置模型为推理模式
-# 这会冻结 BatchNorm 层并禁用 Dropout，保证输出稳定
-model = model.eval()
+img_path = '/media/dioxane/MovieDisk/Dataset/SMVS/business_cards/Canon/001.jpg'
+img = Image.open(img_path).convert('RGB')
 
-data_config = timm.data.resolve_model_data_config(model)
-transforms = timm.data.create_transform(**data_config, is_training=False)
 
-# 5. 处理图片并增加 Batch 维度
-# 输出形状通常为 [1, 3, H, W]
-input_tensor = transforms(img).unsqueeze(0)
+data_config = resolve_model_data_config(model)
+transforms = create_transform(**data_config, is_training=False)
 
-# 6. 提取特征
-# 使用 no_grad 减少显存占用，因为我们不需要反向传播
+input_tensor = transforms(img).unsqueeze(0).to(device)
+
 with torch.no_grad():
     feature_vector = model(input_tensor)
-    for feat in feature_vector:
-        print(feat.shape)
+# for feat in feature_vector:
+#     print(feat.shape)
+for idx, feature in enumerate(feature_vector):
+    feature = feature.permute(0, 2, 3, 1).squeeze(0).cpu().numpy() # [16, 16, 768]
+    pca_features = pca.fit_transform(feature.reshape(-1, feature.shape[-1])).reshape(16, 16, 3)
+    pca_features = (pca_features - pca_features.min()) / (pca_features.max() - pca_features.min())
 
-# 7. 查看结果
-# print(f"模型架构: {model.default_cfg['architecture']}")
-# print(f"特征向量形状: {feature_vector.shape}") 
-# 对于 ViT，通常输出是 [1, 768]
+    plt.figure(figsize=(5, 5))
+    plt.imshow(pca_features)
+    plt.axis('off')
+    plt.title(f'Feature Map - Layer -{idx+1}') # 倒数第几层
+    save_name = f'feature_vis_layer_{idx+1}.png'
+    plt.savefig(save_name, bbox_inches='tight', pad_inches=0)
+    plt.close()
+    print(f"已保存可视化图片: {save_name}")
+    
